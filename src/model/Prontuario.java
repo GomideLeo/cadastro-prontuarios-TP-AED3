0185be6f37b59e0f6a074e2d9ae89131bdb6c126
Leonardo Gomide <leocgomide@gmail.com>
2021-08-19 20:49:28 -0300

    prontuario 2

--- src/model/Prontuario.java
+++ src/model/Prontuario.java
@@ -5,38 +5,85 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.Date;
 
-public class Pessoa {
+public class Prontuario {
+
     protected int codigo;
     protected String nome;
-    protected byte idade;
-    protected float salario;
+    protected Date dataNascimento;
+    protected char sexo;
+    protected String anotacoes;
+
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return this.dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public char getSexo() {
+        return this.sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getAnotacoes() {
+        return this.anotacoes;
+    }
+
+    public void setAnotacoes(String anotacoes) {
+        this.anotacoes = anotacoes;
+    }
     
-    public Pessoa() {
-        codigo = -1;
-        nome = "";
-        idade = (byte)0;
-        salario = 0f;
+    public Prontuario() {
+        this.codigo = -1;
+        this.nome = "";
+        this.dataNascimento = new Date(0);
+        this.sexo = '\0';
+        this.anotacoes = "";
     }
 
-    public Pessoa(int c, String n, byte i, float s) {
-        codigo = c;
-        nome = n;
-        idade = i;
-        salario = s;
+    public Prontuario(int codigo, String nome, Date dataNascimento, char sexo, String anotacoes) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.anotacoes = anotacoes;
     }
-
+    
+    @Override
     public String toString() {
         return "\nCódigo: " + codigo +
                "\nNome: " + nome +
                "\nIdade: " + idade +
                "\nSalario: " + salario;
     }
-
+    
     public byte[] getByteArray() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
         DataOutputStream dos = new DataOutputStream(baos);
-
+        
         try {
             dos.writeInt(codigo);
             dos.writeUTF(nome);
