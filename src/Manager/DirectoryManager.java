e1ca50f1632cb89a6747cbc12f9c4ae4687eaa1a
henrique.castro <henrique.castro@pattrol.com.br>
2021-09-25 11:06:20 -0300

    implements directoryManager

--- src/Manager/DirectoryManager.java
+++ src/Manager/DirectoryManager.java
@@ -1,5 +1,75 @@
 package manager;
 
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.io.File;
+import java.io.IOException;
+
 public class DirectoryManager {
-    
+    private String filePath;
+
+    public DirectoryManager(String filePath) throws FileNotFoundException {
+        this.filePath = filePath;
+        File f = new File(filePath);
+        if (!f.exists()) {
+            throw new FileNotFoundException();
+        }
+    }
+
+    public DirectoryManager(String filePath, int profundidade) throws FileNotFoundException {
+        this.filePath = filePath;
+        File f = new File(filePath);
+        if (!f.exists()) {
+            f.delete();
+        }
+        writeHeader(profundidade);
+    }
+
+    public void writeHeader(int profundidade) throws FileNotFoundException {
+        try {
+            RandomAccessFile arquivo = new RandomAccessFile(filePath, "rw");
+            arquivo.seek(0);
+            arquivo.writeInt(profundidade);
+            arquivo.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void writeToFile(byte[] data) throws IOException {
+        try {
+            RandomAccessFile arquivo = new RandomAccessFile(filePath, "rw");
+            arquivo.seek(0);
+            arquivo.write(data);
+            arquivo.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] readToFile() throws Exception {
+        byte[] data = null;
+        try {
+            RandomAccessFile arquivo = new RandomAccessFile(filePath, "r");
+            data = new byte[(int) arquivo.length()];
+            arquivo.seek(0);
+            arquivo.read(data);
+            arquivo.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    // public Directory initDir(int profundidade) {
+    //     Directory emptyDir = null;
+    //     try {
+    //         emptyDir = new Directory(profundidade);
+    //         writeDir(emptyDir.toByteArray());
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    //     return emptyDir;
+    // }
 }
--- src/model/Directory.java
+++ src/model/Directory.java
@@ -1,5 +1,11 @@
 package model;
 
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+
 public class Directory {
     private int profundidade;
     private int buckets[];
@@ -29,14 +35,23 @@ public class Directory {
         this.buckets = newBuckets;
     }
 
+    public void setProfundidade(int profundidade){
+        this.profundidade = profundidade;
+    }
+
     public int getProfundidade(){
         return this.profundidade;
+    }
+
+    public void setBucket(int bucketAddress, int posicao){
+        this.buckets[posicao] = bucketAddress;
     }
 
     public int getBucket(int posicao){
         return this.buckets[posicao];
     }
 
+    @Override
     public String toString(){
         String aux = "";
         for(int i : this.buckets){
@@ -44,6 +59,36 @@ public class Directory {
         }
         aux += "\n";
         return aux;
+    }
+
+    public byte[] toByteArray() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+
+        try {
+            dos.writeInt(this.getProfundidade());
+            for(int bucket : this.buckets){ 
+                dos.writeInt(bucket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return baos.toByteArray();
+    }
+
+    public void readFromByteArray(byte[] v) {
+        ByteArrayInputStream bais = new ByteArrayInputStream(v);
+        DataInputStream dis = new DataInputStream(bais);
+        
+        try {
+            this.setProfundidade(dis.readInt());
+            for(int i = 0; i < Math.pow(2, this.profundidade); i++) {
+                this.setBucket(dis.readInt(), i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
